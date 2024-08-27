239f5cf32f2494ed96209e6dd481aa132562885c
Good morning yawl <146108693+Snaftt@users.noreply.github.com>
2024-08-27 10:30:43 -0500

    yes

--- obj/Debug/net8.0/ObjectsAndStrings.AssemblyInfo.cs
+++ obj/Debug/net8.0/ObjectsAndStrings.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObjectsAndStrings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ef9813aef44a237fd48f8bd84818cfeb550101")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObjectsAndStrings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObjectsAndStrings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
