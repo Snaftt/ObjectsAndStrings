95ef9813aef44a237fd48f8bd84818cfeb550101
Good morning yawl <146108693+Snaftt@users.noreply.github.com>
2024-08-27 10:30:15 -0500

    Initial commit

---
+++ IBootUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsAndStrings
+{
+    internal interface IBootUp
+    {
+        public bool InOn { get ; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ObjectsAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myComputer = new Computer("Apple", "All-in-One", true);
+            IBootUp myBootUp = myComputer;
+            Console.WriteLine(myComputer.Equals(myBootUp));
+
+            Computer myOtherComputer = new Computer("Apple", "All-in-One", true);
+            Console.WriteLine(myComputer.Equals(myOtherComputer));
+
+            string myString = myComputer.ToString();
+            Console.WriteLine(myString == myComputer.ToString());
+            Console.WriteLine(myString == myOtherComputer.ToString());
+
+            Console.WriteLine(myComputer.GetType());
+            Console.WriteLine(myBootUp.GetType());
+
+            myOtherComputer.PowerOnOff();
+
+            Console.WriteLine(myString == myOtherComputer.ToString());
+
+            Console.WriteLine(myString.Contains('i'));
+            Console.WriteLine(myString.ToUpper());
+        }
+    }
+}
